e0c5a816d2fd2b24e1e8f6b6c4649f4c5df84567
martinlehofer <martin.lehofer@gmail.com>
2015-06-15 23:37:47 -0400

    initial commit

--- cachematrix.R
+++ cachematrix.R
@@ -1,6 +1,7 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 # foo bar
+# bar foo
 ## Write a short comment describing this function
 
 makeCacheMatrix <- function(x = matrix()) {
