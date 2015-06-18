063da4cbb46159d0ea88e9df4cf878a632248ad7
martinlehofer <martin.lehofer@gmail.com>
2015-06-17 22:47:04 -0400

    implemented assignment 2

--- cachematrix.R
+++ cachematrix.R
@@ -1,16 +1,39 @@
 ## Put comments here that give an overall description of what your
 ## functions do
-# foo bar
-# bar foo
-## Write a short comment describing this function
 
+
+## Creates a function(environment) that can store the matrix and also the
+### getters and setters for the inverse of thematrix
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m
+        list(set = set, get = get,
+             setinverse = setinverse,
+             getinverse = getinverse)
 }
 
 
-## Write a short comment describing this function
-
+## Returns a inversed matrix. Function must be created with the function
+### makeCacheMatrix. This function stores the inversed matrix in the 
+### $m object of the environment created by the constructor function 
+### makeCacheMatrix(). If function has been called  before for an 
+### environment this function returns the stored value instead of
+### solving the matrix again.
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
+        m <- x$getinverse()
+        if(!is.null(m)) {
+                print("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
 }
